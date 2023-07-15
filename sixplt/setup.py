ef3174c68093b34eea3b81f09f1324aede60a38d
ctorney <colin.j.torney@gmail.com>
2023-07-15 17:56:54 +0100

    Create setup.py

---
+++ sixplt/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(  name= 'sixplt', 
+        version='0.0.1', 
+        description='Inline matplotlib plots in the terminal with sixel', 
+        py_modules=["sixplt"],
+        package_dir={'': 'src'},
+        install_requires = ["matplotlib", "imagemagick"],
+        
+    )
