3d4974c12c1cab4266a9b51b1fb528a309048e39
ctorney <colin.j.torney@gmail.com>
2023-07-15 19:42:10 +0100

    Update sixplt.py

--- sixplt/src/sixplt.py
+++ sixplt/src/sixplt.py
@@ -1,31 +1,30 @@
 import matplotlib
 import matplotlib.pyplot as plt
-
-
 from subprocess import Popen, PIPE
+import warnings
 
 def is_ipython() -> bool:
     try:
         shell = get_ipython().__class__.__name__
-        print(shell)
-        if shell == 'ZMQInteractiveShell':
-            return False   # Jupyter notebook or qtconsole
-        elif shell == 'TerminalInteractiveShell':
-            return True  # Terminal running IPython
+        if shell == 'TerminalInteractiveShell':
+            return True  
         else:
-            return False  # Other type (?)
+            return False  
     except NameError:
-        return False      # Probably standard Python interpreter
+        return False      
 
+def show(*args, transparent=False, **kwargs):
+    if is_ipython():
+        print()
 
-plt.plot([1, 2, 3])
-if is_ipython():
-    print()
+        try:
+            p = Popen(["convert", 'png:-', 'sixel:-'], stdin=PIPE)
+            plt.savefig(p.stdin, bbox_inches="tight", format='png', transparent=transparent) 
+            p.stdin.close()
+            p.wait()
+        except FileNotFoundError:
+            warnings.warn("Unable to convert plot to sixel format: Imagemagick not found.")
 
-    p = Popen(["convert", 'png:-', 'sixel:-'], stdin=PIPE)
-    plt.savefig(p.stdin, bbox_inches="tight", format='png', transparent=True) 
-    p.stdin.close()
-    p.wait()
-else:
-    plt.show()
+    else:
+        plt.show(*args, **kwargs)
 
